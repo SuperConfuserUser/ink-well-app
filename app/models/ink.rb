4e1a258d4b81203a56441728a668868281108e07
Chely Ho <adept_inept@yahoo.com>
2018-03-23 02:03:10 +0000

    created pen and ink models

--- app/models/ink.rb
+++ app/models/ink.rb
@@ -1,2 +1,5 @@
 class Ink < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :ink_brand
+  has_many :color_families, through: :ink_color_families
 end
--- app/models/pen.rb
+++ app/models/pen.rb
@@ -1,2 +1,6 @@
 class Pen < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :pen_brand
+  belongs_to :pen_type
+  has_one :ink  #doesn't feel right... should ink belong to pen and add an pen_id to inks??
 end
