e536c072c433e00645880abbdb3cf9d47c39dab8
Chely Ho <adept_inept@yahoo.com>
2018-03-23 15:01:27 +0000

    blocked in html and form actions for pens

--- app/controllers/inks_controller.rb
+++ app/controllers/inks_controller.rb
@@ -1,37 +1,37 @@
 class InksController < ApplicationController
 
   # GET: /inks
-  get "/inks" do
+  get "/inks/?" do
     erb :"/inks/index.html"
   end
 
   # GET: /inks/new
-  get "/inks/new" do
+  get "/inks/new/?" do
     erb :"/inks/new.html"
   end
 
   # POST: /inks
-  post "/inks" do
+  post "/inks/?" do
     redirect "/inks"
   end
 
   # GET: /inks/5
-  get "/inks/:id" do
+  get "/inks/:id/?" do
     erb :"/inks/show.html"
   end
 
   # GET: /inks/5/edit
-  get "/inks/:id/edit" do
+  get "/inks/:id/edit/?" do
     erb :"/inks/edit.html"
   end
 
   # PATCH: /inks/5
-  patch "/inks/:id" do
+  patch "/inks/:id/?" do
     redirect "/inks/:id"
   end
 
   # DELETE: /inks/5/delete
-  delete "/inks/:id/delete" do
+  delete "/inks/:id/delete/?" do
     redirect "/inks"
   end
 end
