e18d200765e2a69d7b29878df85bed78de9129c0
Chely Ho <adept_inept@yahoo.com>
2018-03-23 18:59:28 +0000

    created signup or  user/new actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,30 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  get '/login/?' do
+    erb :welcome
+  end
+
+  get '/logout/?' do
+    redirect '/login'
+  end
+
+  post '/login/?' do
+
+  end
+
+  helpers do
+		def logged_in?
+			!!current_user
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+
+    def validate?
+      !self.any?{|obj| obj.emtpy?}
+    end
+	end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,34 @@
 class UsersController < ApplicationController
 
-  # GET: /users
   get "/users/?" do
     erb :"/users/index.html"
   end
 
-  # GET: /users/new
   get "/users/new/?" do
     erb :"/users/new.html"
   end
 
-  # POST: /users
   post "/users/?" do
-    redirect "/users"
+    if !params[:user].values.any?(&:empty?)
+      @user = User.create(params[:user])
+      redirect "/users/#{@user.id}"
+    else
+      redirect "/users/new"
+    end
   end
 
-  # GET: /users/5
   get "/users/:id/?" do
     erb :"/users/show.html"
   end
 
-  # GET: /users/5/edit
   get "/users/:id/edit/?" do
     erb :"/users/edit.html"
   end
 
-  # PATCH: /users/5
   patch "/users/:id/?" do
     redirect "/users/:id"
   end
 
-  # DELETE: /users/5/delete
   delete "/users/:id/delete/?" do
     redirect "/users"
   end
