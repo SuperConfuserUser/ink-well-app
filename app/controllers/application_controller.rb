f3a4eb8d8435e5fd087fa63eb07dcb1272709c23
Chely Ho <adept_inept@yahoo.com>
2018-03-25 04:04:08 +0000

    updated some login based details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,6 +41,10 @@ class ApplicationController < Sinatra::Base
 		def current_user
 			@current_user ||= User.find(session[:user_id]) if session[:user_id]
 		end
+
+    def document
+      document
+    end
 	end
 
 end
