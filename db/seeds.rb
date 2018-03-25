e986514075fd65cc2660688a9c03ac0a22247a79
Chely Ho <adept_inept@yahoo.com>
2018-03-25 03:38:24 +0000

    update seed with color families and fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,47 @@
-pen_types = ["fountain", "gel", "rollerball", "ballpoint", "felt tip", "brush"]
+pen_types = [
+  "fountain",
+  "gel",
+  "rollerball",
+  "ballpoint",
+  "felt tip",
+  "brush"]
 
 pen_types.each {|t| PenType.create(name: t)}
+
+
+
+color_families = [
+  "black",
+  "blue",
+  "red",
+  "yellow",
+  "green",
+  "orange",
+  "purple",
+  "pink",
+  "gray",
+  "brown",
+  "dark",
+  "light",
+  "shimmer",
+  "metallic",
+  "other"
+]
+
+color_families.each {|c| PenType.create(name: c)}
+
+
+User.create!(username: 'foo',
+             email: 'foo@bar.com',
+             password_digest: BCrypt::Password.create('password'))
+
+User.create!(username: 'user',
+             email: 'user@internet.com',
+             tagline: 'I exist everywhere.',
+             password_digest: BCrypt::Password.create('password'))
+
+User.create!(username: 'TheBard',
+             email: 'thebard@theatre.com',
+             name: 'William Shakespeare',
+             tagline: 'The pen is mightier than the sword.',
+             password_digest: BCrypt::Password.create('password'))
