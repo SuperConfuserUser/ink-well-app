2519431c35584206f5b2f1607cc2223be674d6cf
Chely Ho <adept_inept@yahoo.com>
2018-03-28 04:13:46 +0000

    update action working

--- app/controllers/inks_controller.rb
+++ app/controllers/inks_controller.rb
@@ -27,11 +27,24 @@ class InksController < ApplicationController
   end
 
   get "/inks/:id/edit/?" do
+    @ink = Ink.find(params[:id])
+    redirect "/inks" if !@ink
     erb :"/inks/edit.html"
   end
 
   patch "/inks/:id/?" do
-    redirect "/inks/:id"
+    @ink = Ink.find(params[:id])
+    redirect "/inks" if !@ink
+
+    valid = @ink.user == current_user && ![params[:brand], params[:ink][:name]].any?(&:empty?)
+    redirect "/inks/#{@ink.id}/edit" if !valid
+
+    @ink.update(params[:ink])
+    @ink.favorite = params[:ink][:favorite]
+    @ink.ink_brand = InkBrand.find_or_create_by(name: params[:brand])
+    @ink.color_family_ids = params[:color_families]
+
+    redirect @ink.save ? "/inks/#{@ink.id}" : "/inks/#{@ink.id}/edit"
   end
 
   delete "/inks/:id/delete/?" do
